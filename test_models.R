f5d6d701f57abb7459af527410d1c51172c8255f
mseewaters <mseewaters@gmail.com>
2014-09-14 13:03:15 -0400

    beginning modle build

--- test_models.R
+++ test_models.R
@@ -1,3 +1,7 @@
+library(DMwR)
+library(ggplot2)
+library(e1071)
+library(performanceEstimation)
 
 
 # Data Understanding/Preparation
@@ -12,19 +16,19 @@
 
 # loadData ----------------------------------------------------------------
 ## Load the data into R
-data <- read.csv("D:/0 Stern MSBA/0.2 Abduction/abduction2/Sample_data.csv", stringsAsFactors=TRUE, na.strings = c("NA",""," "))
+data.raw <- read.csv("D:/0 Stern MSBA/0.2 Abduction/abduction2/Sample_data.csv", stringsAsFactors=TRUE, na.strings = c("NA",""," "))
 names.data <- c("Date","Victim.Age","Victim.Race","Victim.Gender","Harm","Publicized","Location","Region",
                 "Relationship","RSO","Offender.Age","Offender.Race","Offender.Gender","Rural.City","Missing.Location",
                 "Recovery.Location", "Recovery", "Number.Victims")
-colnames(data) <- names.data
-summary(data)
+colnames(data.raw) <- names.data
+summary(data.raw)
 
 
 # missingData -------------------------------------------------------------
 ## Evaluate missing data and determine best method to handle
 
 # Remove lines where victim info and relationship is not provided
-data <- data[complete.cases(data[,c(2,3,4,5,9)]),]
+data <- data.raw[complete.cases(data.raw[,c(2,3,4,5,9)]),]
 
 # Count NA by variable and isolate fields with at least 1 NA
 data.empty = NULL
@@ -34,7 +38,17 @@ for (i in 1:length(data))
 }
 x <- as.data.frame(cbind(names.data, data.empty)[order(-data.empty),])
 x <- x[which(x$data.empty != 0),]
+x
 
+# Remove Recovery.Location and Missing.Location
+# Almost half of observations missing data
+data <- data[,-which(names(data) %in% c("Recovery.Location","Missing.Location"))]
+
+# Fill RSO and Rural.City using nearest neighbors
+data <- knnImputation(data, k=3)
+check <- nrow(data[!complete.cases(data),])
+
+summary(data)
 
 # additionalVariables -----------------------------------------------------
 ## Create additional variable groups as needed
@@ -52,6 +66,7 @@ table(data$Victim.AgeGroup)
 harm.list <- as.data.frame(unique(unlist(data$Harm), use.names=FALSE))
 colnames(harm.list) <- c("Harm")
 harm.list$target <- c(0,1,1,1,0,1)
+harm.list$target2 <- c("NoHarm","Harm","Harm","Harm","NoHarm","Harm")
 data <- merge(data, harm.list, by = "Harm")
 table(data$target)
 
@@ -87,7 +102,38 @@ table(data$target)
 # modelBuild --------------------------------------------------------------
 ####  CREATE MODELS HERE
 
+# Remove Harm (text), Date, Location (17 factors for 20 incidents), Recovery (not known at time)
+data.m <- data[,-which(names(data) %in% c("Harm","Date","Location", "Recovery","target2"))]
+data.m2 <- data[,-which(names(data) %in% c("Harm","Date","Location", "Recovery","target"))]
 
+model.tree <- rpartXse (target ~ ., data=data.m)
+pred.tree <- predict(model.tree, data.m)
+
+model.svm <- svm(target ~ ., data=data.m)
+pred.svm <- predict(model.svm, data.m)
+
+model.nb <- naiveBayes(target2 ~ ., data.m2)
+data.ms.test <- data.m2[,1:13]
+pred.nb <- predict(model.nb, data.ms.test, type="raw")
+
+
+res.test <- performanceEstimation(
+  PredTask(target ~ ., data.m),
+  workflowVariants("standardWF", learner = "naiveBayes", learner.pars=list(type="raw")),
+  CvSettings(nReps =1, nFolds = 10))
+summary(res.test)
+plot(res.test)
+
+
+res <- performanceEstimation(
+  PredTask(target ~ ., data.m),
+  c(workflowVariants("standardWF", learner = "svm",
+           learner.pars=list(cost=c(1,10), gamma=c(0.1,0.01))),
+    workflowVariants("standardWF", learner = "rpartXse",
+                     learner.pars=list(se=c(0,1)))),
+  CvSettings(nReps = 2, nFolds = 10))
+summary(res)
+plot(res)
 
 # modelEvaluate -----------------------------------------------------------
 #### EVALUATE MODELS HERE
